c14ff7f3977dcfa76a9d826b64a1abd85f7eb3d5
James Trinklein <james.trinklein@changepoint.com>
2014-11-13 15:06:16 -0800

    quick and dirty tabs first draft

--- assets/app/main.js
+++ assets/app/main.js
@@ -2,7 +2,9 @@ var codeMirrors = [],
 	last = new Date(),
 	state = null;
 
+window.updateTabs = function() {
 
+};
 window.onload = function() {
 
 	/**
@@ -17,34 +19,42 @@ window.onload = function() {
 	 * Initialize lessons from previous session
 	 */
 	state = localStorage.getItem("newState");
-	if (state) {
+/*	if (state) {
 		state = JSON.parse(state);
 		var firstUnfinishedQuestion;
 
 		$(".lesson").each(function(cnt,item) {
+			$('.lesson-link-'+cnt).show();
+
 			var go = $(".go", item)[0],
 				code = $(".code", item)[0],
 				post = $(".post", item)[0];
 
 			if (cnt < state.answers.length) {
 				$(code).val(state.answers[cnt]);
-				item.style.visibility = "visible";
+				//item.style.visibility = "visible";
+				$('#lesson-' + (cnt+1)).addClass('lesson-tab');
 				if (post !== undefined) {
-					post.style.visibility = "visible";
+					//post.style.visibility = "visible";
 				}
 			}
 			else if (cnt === state.answers.length) {
-				item.style.visibility = "visible";
+				//item.style.visibility = "visible";
 			}
 		});
 	}
-
+*/
 	/**
 	 * Initialize lessons
 	 */
 	var lessons = $(".lesson");
-
+	lessons.hide();
+	lessons.addClass('lesson-tab');
 	lessons.each(function(cnt, item) {
+		var me = $(item);
+		me.attr('id', 'lesson-'+cnt);
+
+
 		var go = $(".go", item)[0],
 			output = $(".output", item)[0],
 			code = $(".code", item)[0],
@@ -80,12 +90,12 @@ window.onload = function() {
 					codeMirror.save();
 					saveAllAnswers();
 					verifier($(code).val(), item);
-
 					if (post !== undefined) {
-						post.style.visibility = "visible";
+						//post.style.visibility = "visible";
 					}
 					if (cnt < lessons.length - 1) {
-						lessons[cnt + 1].style.visibility = "visible";
+						//lessons[cnt + 1].style.visibility = "visible";
+						$('.lesson-link-' + (cnt+1)).show();
 					}
 
 				} catch (ex) {
@@ -111,4 +121,18 @@ window.onload = function() {
 		}
 
 	});
+	$("#tablist").empty();
+	$("#tablist").append('<li><a href="#intro" >Introduction</a></li>');
+	$('.lesson-tab').each(function(index,item) {
+		$("#tablist").append('<li class="lesson-link lesson-link-' + index + '"><a href="#' + item.id + '" >Lesson ' + index + '</a></li>');
+		$('.lesson-link-' + index).click(function(){
+			codeMirrors[index].setSize();
+		});
+	});
+
+	$( "#content" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+	$( "#tablist li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+    $('.lesson-link').hide();
+    $('.lesson-link').first().show();
+
 }
